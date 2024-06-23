0c80c5b2fe877dff3df7857732d15cb28691f9df
Shayan <shayan83bajelan@gmail.com>
2024-06-23 21:55:41 +0330

    Adding linked list instead of a regular list, to easly append, remove, search through it Search, Append, At are new methods, Node_typename and Linked_List_typename are structs which handle the process

--- Libraries/list.c
+++ Libraries/list.c
@@ -1,8 +1,27 @@
 #include "list.h"
 
-int main() {
-    List(int, p);
-    Append(2, p);
+DLinkedList(string, char *);   
 
-    printf("%d\n", p->length);
+bool isInNode(char *value, Node_string* current) {
+    if (current->value == value) return true;
+    return false;
+}
+
+int main() {
+    LList(string, names);
+
+    Append((char *) "shayan1", names);
+    Append((char *) "shayan2", names);
+    Append((char *) "shayan3", names);
+    Append((char *) "shayan4", names);
+
+    Node_string* answer = Search("shayan3", names, isInNode);    
+    printf("%s\n", answer->value);
+
+    // Append((char *) "shayan", names);
+    // Append((char *) "erfan", names);
+    // Append((char *) "erfan", names);
+    // Append((char *) "erfan", names);
+
+    // printf("%d\n", names->items->value);
 }
--- Libraries/list.h
+++ Libraries/list.h
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "utilities.h"
 
 #define GLUE_HELPER(x, y) x##y
@@ -19,6 +17,65 @@
 #define RA(type, ptr, size) \
     (type *) realloc(ptr, size * sizeof(type))
 
-#define Append(newItem, parent) *(parent->items + parent->length) = newItem; parent->items = RA(T(newItem), parent->items, (parent->length + 2)); parent->length += 1;
+#define DLinkedList(typeName, type) typedef struct GLUE(Node_, typeName) { type value; struct GLUE(Node_, typeName) *next; } GLUE(Node_, typeName); typedef struct GLUE(Linked_List_, typeName) { int length; GLUE(Node_, typeName) *items; } GLUE(Linked_List_, typeName);
+#define NewNode(newItem, type) \
+({\
+    type* newNode = CA(type);\
+    newNode->value = newItem;\
+    newNode->next = NULL;\
+    newNode;\
+})
+#define LList(typeName, name) GLUE(Linked_List_, typeName) *name = MA(GLUE(Linked_List_, typeName)); name->length = 0; name->items = CA(GLUE(Node_, typeName));
+#define Append(newItem, lls)                                                  \
+({                                                                            \
+    T(lls->items) head = lls->items;                                          \
+    T(lls->items) newNode = NewNode(newItem, T(*(lls->items)));               \
+    if (lls->length == 0) {                                                   \
+        head->value = newNode->value;                                         \
+        head->next = NULL;                                                    \
+    }                                                                         \
+    else{                                                                     \
+        T(lls->items) last = head;                                            \
+        while (last->next != NULL) last = last->next;                         \
+        last->next = newNode;                                                 \
+    }                                                                         \
+    lls->length += 1;                                                         \
+})
 
-#define List(type, name) typedef struct GLUE(List_, name) { int length; type *items; } GLUE(List_, name); GLUE(List_, name) GLUE(List_Object_, name); GLUE(List_, name) *name = &GLUE(List_Object_, name); name->length = 0; name->items = CA(type);
+#define At(index, lls)                \
+({                                    \
+    T(lls->items) head = lls->items;  \
+    T(lls->items) answer = head;      \
+    if (lls->length == 0) {           \
+        answer = NULL;                \
+    } else {                          \
+        int i = 0;                    \
+        while (head != NULL) {        \
+            if (i == index) {         \
+                answer = head;        \
+                break;                \
+            }                         \
+            i += 1;                   \
+            head = head->next;        \
+        }                             \
+    }                                 \
+    answer;                           \
+})
+
+#define Search(value, lls, keyFunc)                                     \
+({                                                                      \
+    T(lls->items) head = lls->items;                                    \
+    T(lls->items) answer = NewNode("Not found!", T(*(lls->items)));     \
+    while (head != NULL) {                                              \
+        if (keyFunc(value, head)) {                                     \
+            answer = head;                                              \
+            break;                                                      \
+        }                                                               \
+        head = head->next;                                              \
+    }                                                                   \
+    answer;                                                             \
+})                                                                      \
+
+#define DList(typeName, type) typedef struct GLUE(List_, typeName) { int length; type *items; } GLUE(List_, typeName);
+#define List(typeName, name) GLUE(List_, typeName) *name = MA(GLUE(List_, typeName)); name->length = 0; name->items = CA(T(*(name->items)));
+#define LsAppend(newItem, parent) *(parent->items + parent->length) = newItem; parent->items = RA(T(newItem), parent->items, (parent->length + 2)); parent->length += 1;
